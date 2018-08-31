cb9ee791899c57a35fefd0b15d81498978b22c70
Youssef Elabd <ykelabd@gmail.com>
2018-08-31 17:19:32 -0400

    Initial Commit

---
+++ TipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by Youssef Elabd on 8/31/18.
+//  Copyright © 2018 Youssef Elabd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
